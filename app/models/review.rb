6b809e688a8c1036e9c6c5f878a62ac12b4dac4e
anonymous anonymous <caleb.baltes@kellogg.northwestern.edu>
2021-12-06 17:41:39 +0000

    Direct association between users and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  belongs_to :reviewer,
+             :class_name => "User"
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :reviews,
+             :foreign_key => "reviewer_id",
+             :dependent => :destroy
+
   has_many   :reservations,
              :dependent => :destroy
 
