5d1b9968189ab3cb31d85bdec44c6923df512d68
anonymous anonymous <caleb.baltes@kellogg.northwestern.edu>
2021-12-06 17:42:00 +0000

    Whitelist columns in Vehicle for ActiveAdmin

--- app/admin/vehicles.rb
+++ app/admin/vehicles.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Vehicle do
 
+ permit_params :make, :model, :width, :height
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
