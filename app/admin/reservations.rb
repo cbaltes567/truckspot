20bcbee6d8b3b17431373301a8194fcc9e14cb69
anonymous anonymous <caleb.baltes@kellogg.northwestern.edu>
2021-12-06 17:41:55 +0000

    Whitelist columns in Reservation for ActiveAdmin

--- app/admin/reservations.rb
+++ app/admin/reservations.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Reservation do
 
+ permit_params :user_id, :garage_id, :start_date, :end_time
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
